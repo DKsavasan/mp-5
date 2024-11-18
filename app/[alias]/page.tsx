558bb0c7a7ddaf1c054661e05438db390fc123fd
DORUK SAVASAN <doruk@crc-dot1x-nat-10-239-93-108.bu.edu>
2024-11-18 16:11:34 -0500

    bug fix for vercel

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,19 +1,19 @@
+// app/[alias]/page.tsx
 import { redirect } from "next/navigation";
 import getUrlByAlias from "@/lib/getUrlByAlias";
 
-interface PageProps {
-  params: {
-    alias: string;
-  };
-}
-
-export default async function AliasPage({ params }: PageProps) {
+export default async function AliasPage({
+  params,
+}: {
+  params: { alias: string };
+}) {
   const { alias } = params;
   const urlData = await getUrlByAlias(alias);
 
   if (urlData) {
     // Redirect to the original URL
     redirect(urlData.url);
+    return null; // Return null after redirecting
   } else {
     // Handle alias not found
     return (
